8124a2c024733bc176f32132f83733cfa8159247
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-10-14 15:45:14 +0300

    Update buildings panel

---
+++ Defense of the Kingdom/Assets/Scripts/KingdomBuildings/ClosedBuildingsShopPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DoTween
+{
+    public class ClosedBuildingsShopPanel : MonoBehaviour
+    {
+        [SerializeField] private GameObject _buildingsPanel;
+        [SerializeField] private Button _closedBuildingsButton;
+        [SerializeField] private Transform _transformBuildingsPanel;
+
+        private void OnEnable()
+        {
+            _closedBuildingsButton.onClick.AddListener(ClosedBuildingsPanel);
+        }
+
+        private void ClosedBuildingsPanel()
+        {
+            transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+            _buildingsPanel.SetActive(false);
+        }
+    }
+}
