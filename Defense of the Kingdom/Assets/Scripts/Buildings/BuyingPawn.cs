0f9db5dce8926abf2548119476c3d0430f97a549
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-10-15 16:38:27 +0300

    Add Buying Pawn

---
+++ Defense of the Kingdom/Assets/Scripts/Buildings/BuyingPawn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Currensy;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Buildings
+{
+    public class BuyingPawn : MonoBehaviour
+    {
+        [SerializeField] private Button _buyPawnButton;
+        [SerializeField] private GameObject _pawnPrefab;
+        [SerializeField] private Transform _pawnSpawnPoint;
+        [SerializeField] private int _pricePawn = 3;
+        [SerializeField] private  Coins _coins;
+
+        private void OnEnable()
+        {
+            _buyPawnButton.onClick.AddListener(PawnBuy);
+        }
+
+        private void PawnBuy()
+        {
+            if (_coins.value < 3)
+            {
+                Debug.Log("nema zolota");
+            }
+            else
+            {
+                _coins.SpendCurrency(_pricePawn);
+                Instantiate(_pawnPrefab, _pawnSpawnPoint.position, Quaternion.identity);
+            }
+           
+        }
+
+        private void OnDisable()
+        {
+            _buyPawnButton.onClick.RemoveListener(PawnBuy);
+        }
+    }
+}
