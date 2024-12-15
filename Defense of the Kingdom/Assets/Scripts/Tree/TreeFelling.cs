28afb2e6d1203626c54a8775147d9f5dc05647fd
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-12-15 15:40:03 +0200

    Add falling animation for tree

--- Defense of the Kingdom/Assets/Scripts/Tree/TreeFelling.cs
+++ Defense of the Kingdom/Assets/Scripts/Tree/TreeFelling.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,13 +10,26 @@ public class TreeFelling : MonoBehaviour
     [SerializeField] private GameObject _treeButton;
     [SerializeField] private Transform _treeTransform;
     [SerializeField] private GameObject _loot;
-
-    private void OnCollisionEnter2D(Collision2D other)
+    [SerializeField] private AudioSource _fallingTreeFx;
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<PlayerController>())
+        if (other.gameObject.GetComponent<AttackArea>())
         {
-            _treeButton.SetActive(true);
+            Debug.Log("Hit to tree");
+            StartFelling();
         }
+    }
+
+    private void FallingTree()
+    {
+        Sequence animationTree = DOTween.Sequence();
+        animationTree
+            .Append(_treeTransform.DOPunchPosition(Vector3.right * 0.1F, 1f))
+            .Append(_treeTransform.DORotate(new Vector3(0, 0, 90), 1).SetEase(Ease.Linear))
+            .Append(_treeTransform.DORotate(new Vector3(0, 0, 85), 0.2f).SetEase(Ease.Linear))
+            .Append(_treeTransform.DORotate(new Vector3(0, 0, 90), 0.2f).SetEase(Ease.Linear));
     }
 
     public void StartFelling()
@@ -25,6 +39,8 @@ public class TreeFelling : MonoBehaviour
 
     private IEnumerator FellingTree()
     {
+        _fallingTreeFx.Play();
+        FallingTree();
         yield return new WaitForSeconds(3f);
         Instantiate(_loot,_treeTransform.position, Quaternion.identity);
       
